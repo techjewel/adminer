d6ab104b6bc9ecf4e8d07186f98781ab3edbaea8
adreastrian <mahmudulhasanarif@gmail.com>
2018-08-27 11:11:32 +0600

    min width

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('.footer').css({
-        minWidth: $('#content table').first().width() + 1 + 'px'
+        minWidth: $('#content table').first().width() + 20 + 'px'
     });
 });
